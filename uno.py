95f83f885a8e1f993f6f55c46fd39bd5ddfa1601
caloloc2 <calolomino@gmail.com>
2020-12-04 21:13:16 -0500

    actualizacion

--- uno.py
+++ uno.py
@@ -3,73 +3,75 @@ import matplotlib.pyplot as plt
 
 random.seed()
 
-def obtener_parametros_circulo():    
-    Cx = random.randint(4, 6)
-    Cy = random.randint(4, 6)
-    R = random.randint(1, 4)
-    return Cx, Cy, R
+def obtener_parametros_circulo():
+    # genera numeros aleatorios para el centro y el radio del circulo entre los limites establecidos
+    Cx = random.randint(4, 6) # centro en x
+    Cy = random.randint(4, 6) # centro en y
+    R = random.randint(1, 4) # radio
+    return Cx, Cy, R # retorna valores
 
 def obtener_parametros_rectangulo():
-    W = random.randint(1, 4)
-    H = random.randint(1, 4)
-    Ox = random.randint(0, 6)
-    Oy = random.randint(0, 6)
-    return W, H, Ox, Oy 
+    # genera numeros aleatorios para obtener la posicion inicial, ancho y alto del rectangulo
+    W = random.randint(1, 4) # ancho de rectangulo
+    H = random.randint(1, 4) # alto de rectangulo
+    Ox = random.randint(0, 6) # origen en x
+    Oy = random.randint(0, 6) # origen en y
+    return W, H, Ox, Oy # retorna valores
 
 def generacion_puntos(cantidad):
-    point_list = []
-    for x in range(cantidad):
-        x = random.uniform(0,10)
-        y = random.uniform(0,10)
+    # genera puntos aleatorios en toda el area
+    lista_puntos = [] # array de puntos
+    for x in range(cantidad): # itera en numero asignado
+        x = random.uniform(0,10) # obtiene punto en x
+        y = random.uniform(0,10) # obtiene punto en y
         
-        point_list.append([x,y])
+        lista_puntos.append([x,y]) # agrega a array de puntos
 
-    return point_list
+    return lista_puntos # retorna array
 
 def dentro_circulo(punto):
+    # determina si un punto se encuentra dentro del circulo con sus parametros
     return math.sqrt((punto[0] - Cx)**2 + (punto[1] - Cy)**2) <= R
 
 def dentro_rectangulo(punto):
-    retorno = False
+    # determina si un punto se encuentra dentro de un rectangulo
+    retorno = False # variable de retorno
     
+    # si en punto en x se encuentra dentro del origen y el ancho, y el punto en y se encuentra dentro del origen y el alto
     if (((punto[0] > Ox) and (punto[0] < (Ox+W))) and ((punto[1] > Oy) and (punto[1] < (Oy+H)))):
-        retorno = True
+        retorno = True # si cumple la condicion setea como verdadero
     
-    return retorno
+    return retorno # retorna valor
 
 numero_puntos = 2500
-Cx, Cy, R = obtener_parametros_circulo()
-W, H, Ox, Oy = obtener_parametros_rectangulo()
-puntos = generacion_puntos(numero_puntos)
+Cx, Cy, R = obtener_parametros_circulo() # obtiene parametros de circulo
+W, H, Ox, Oy = obtener_parametros_rectangulo() # obtiene parametros de rectangulo
+puntos = generacion_puntos(numero_puntos) # genera puntos dentro del area
 
-fig, ax = plt.subplots()
+fig, ax = plt.subplots() # crea cuadro para grafica
 
-plt.xlim(0, 10)
-plt.ylim(0, 10)
+plt.xlim(0, 10) # limites de la grafica en x
+plt.ylim(0, 10) # limites de la grafica en y
+plt.grid(linestyle='--') # setea linea cortada para ejes
+ax.set_aspect(1) # aspecto cuadrado de grafica
 
-plt.grid(linestyle='--')
+circulo = plt.Circle((Cx, Cy), R, edgecolor='r', facecolor='none') # grafica el circulo
+rectangulo = plt.Rectangle((Ox, Oy), W, H, linewidth=1, edgecolor='b', facecolor='none') # grafica el rectangulo
 
-ax.set_aspect(1)
+ax.add_artist(circulo) # agrega grafica al cuadro
+ax.add_artist(rectangulo) # agrega grafica al cuadro
 
-circulo = plt.Circle((Cx, Cy), R, edgecolor='r', facecolor='none')
-rectangulo = plt.Rectangle((Ox, Oy), W, H, linewidth=1, edgecolor='b', facecolor='none')
+puntos_area = [] # array de puntos en area de interseccion
+for punto in puntos: # recorre todos los puntos generados
+    if (dentro_rectangulo(punto) and dentro_circulo(punto)): # si cumple las dos condiciones => se encuentra dentro del area de interseccion
+        puntos_area.append(punto) # agrega punto al array de puntos
+        ax.add_artist(plt.Circle((punto[0], punto[1]), 0.03, edgecolor='g')) # grafica punto en el area de interseccion en verde
+    else: # si no cumple condicion => no esta dentro del area de interseccion
+        ax.add_artist(plt.Circle((punto[0], punto[1]), 0.03, edgecolor='gray'))  # grafica punto en el area de interseccion en gris
 
-ax.add_artist(circulo)
-ax.add_artist(rectangulo)
+n = len(puntos_area) # numero total de puntos dentro del area de interseccion
+area = n / numero_puntos # calcula el area a partir del numero de puntos dentro del area de interseccion con el total de puntos generados
 
-puntos_area = []
+plt.title('Area encontrada = '+str(area), fontsize=8) # agrega un titulo a la grafica
 
-
-for punto in puntos:    
-    if (dentro_rectangulo(punto) and dentro_circulo(punto)):
-        puntos_area.append(punto)        
-        ax.add_artist(plt.Circle((punto[0], punto[1]), 0.03, edgecolor='g'))    
-    else:
-        ax.add_artist(plt.Circle((punto[0], punto[1]), 0.03, edgecolor='gray'))
-
-n = len(puntos_area)
-area = n / numero_puntos
-
-plt.title('Area encontrada = '+str(area), fontsize=8)
-
-plt.show()
+plt.show() # muestra la grafica
